d15b50c96d568898cf9f518645fcc8974911df13
Niels van Amstel <namstel@gmail.com>
2023-11-24 21:28:42 +0100

    Renaming

--- strapi-server.js
+++ strapi-server.js
@@ -6,7 +6,7 @@ const services = require('./server/services');
 const routes = require('./server/routes');
 const config = require('./server/config');
 const controllers = require('./server/controllers');
-const bundleItems = require('./server/bundle-items');
+const contentTypes = require('./server/content-types');
 
 module.exports = () => {
   return {
@@ -16,6 +16,6 @@ module.exports = () => {
     config,
     controllers,
     services,
-    bundleItems,
+    contentTypes,
   };
 };
